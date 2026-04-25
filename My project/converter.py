bd1db2e117e0c731122aea6879e038f95a0a7e07
Zapzap <zuzannaa.pietrzak@gmail.com>
2026-04-25 13:29:18 +0200

    Updated converter with followups, added newest dialogues + conversion

--- My project/converter.py
+++ My project/converter.py
@@ -1,27 +1,42 @@
 import json
+import re
 
-def purgeHTML (txt: str) -> str:
-    return txt.replace("<p>", "").replace("</p>", "")
+reg_search = r'\[\w*\]'
+
+IN  = "in.json"
+OUT = "Assets/Dialogues/Dialogue.json"
+
+def purgeHTML (txt: str | None) -> str | None:
+    # purges HTML & Wordle brackets
+    if txt is None: return None # prevents conversion of None
+    return txt.replace("<p>", "").replace("</p>", "").replace("[", "").replace("]", "")
+
+def purgeHTMLRegEx (txt: re.Match | None) -> str | None:
+    if txt is None: return None
+    else:
+        return purgeHTML(txt.group(0))
 
 class Choice:
-    def __init__(self, msg, paragon, renegade):
-        self.msg = msg
-        self.par = paragon
-        self.ren = renegade
+    def __init__(self, msg, followup, paragon, renegade):
+        self.msg      = msg
+        self.followup = followup
+        self.par      = paragon
+        self.ren      = renegade
 
     def toDict(self) -> dict:
         return {
             "text":          self.msg,
             "plus_paragon":  self.par,
-            "plus_renegade": self.ren
+            "plus_renegade": self.ren,
+            "follow_up":     self.followup
         }
 
 class Message:
-    def __init__(self, mid, person, msg, next, choices):
+    def __init__(self, mid, person, msg, wordle, next, choices):
         self.id      = mid
         self.person  = person # 'title'
         self.msg     = msg    # 'content'
-        self.wordle  = "" # TODO
+        self.wordle  = wordle
         self.next    = next
         self.choices = choices
 
@@ -40,7 +55,7 @@ class Message:
 
 msgs = [] # : list[Message]
 
-with open('in.json', 'r') as f:
+with open(IN, 'r') as f:
     jin = json.load(f)
 
 queue = [jin["startingElement"]] # currently checked var(s), starting from first
@@ -53,27 +68,32 @@ while True: # broken only explicitly
     msg_choices = []
     msg_next    = ""
 
+    # message manager skips choice msgs and followup msgs, rendering their `== 1` not needing of distinguishing
     if len(elem["outputs"]) == 0: break # explicit end
     elif len(elem["outputs"]) == 1:
         msg_next = conns[elem["outputs"][0]]["targetid"] # immediate next message
-    elif len(elem["outputs"]) > 1: # choice message type
-        msg_next = conns[elem["outputs"][0]]["targetid"] # nested next message ID
-        msg_next = conns[elems[msg_next]["outputs"][0]]["targetid"] # ...goes one msg deeper, leaping over choice
+    elif len(elem["outputs"]) > 1: # choice message type (-> indicates what msg it points towards)
+        msg_next = conns[elem["outputs"][0]]["targetid"] # nested next message ID (-> choice)
+        msg_next = conns[elems[msg_next]["outputs"][0]]["targetid"] # ...goes one msg deeper, leaping over choice (-> followup)
+        msg_fup  = msg_next # followup ID, to be used later
+        msg_next = conns[elems[msg_next]["outputs"][0]]["targetid"] # ...and once more, leaping over followup (-> actual nxt msg)
         for choice in elem["outputs"]:
-            tit = purgeHTML(elems[conns[choice]["targetid"]]["title"]) # title is split later
+            tit = purgeHTML(elems[msg_fup]["title"]) # title is split to get paragon/renegade stuff
             tits = tit.split(";")
             msg_choices.append(Choice(msg      = purgeHTML(elems[conns[choice]["targetid"]]["content"]),
+                                      followup = purgeHTML(elems[msg_fup]["content"]),
                                       paragon  = int(tits[1]),
                                       renegade = int(tits[2])))
 
     msgs.append(Message(mid     = queue[0],
                         person  = purgeHTML(elem["title"]),
                         msg     = purgeHTML(elem["content"]),
+                        wordle  = purgeHTMLRegEx(re.search(reg_search, elem["content"])),
                         next    = msg_next, # msg_next based on first connection, even with multiple
                         choices = msg_choices))
     queue.pop(0) # removes analysed item
     for outp in elem["outputs"]:
-        queue.append(conns[outp]["targetid"]) # refill of queue
+        queue.append(msg_next) # refill of queue
     if len(queue) == 0: break # breaks out of loop
 
 msg_dictified = [] # : list[dict]
@@ -91,5 +111,5 @@ data = {
   "nodes": msg_dictified
 }
 
-with open("outb.json","w") as f:
+with open(OUT,"w") as f:
     json.dump(data, f, indent=2)
